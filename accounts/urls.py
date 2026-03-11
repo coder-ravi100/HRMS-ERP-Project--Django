72a40d1ba00e59d4a007fd0fc3e87088ee00a9da
coder-ravi100 <ravirathod100100@gmail.com>
2026-03-11 13:55:06 +0530

    owner Admin and ther files created

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.login, name='login')
+   path("", views.login_view, name="login_view"),
+   path("admin/dashboard/", views.admin_dashboard, name = "admin_dashboard"),
+   path("hr/dashboard/", views.hr_dashboard, name = "hr_dashboard"),
+   path("manager/dashboard/", views.manager_dashboard, name = "manager_dashboard"),
+   path("employee/dashboard/", views.employee_dashboard, name = "employee_dashboard"),
+   
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,35 +1,53 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
-def login(request):
-    return render(request,'accounts/login,html')
-
-
-
 def login_view(request):
-
     if request.method == "POST":
-
         username = request.POST.get("username")
         password = request.POST.get("password")
 
-        user = authenticate(request,username=username,password=password)
+        user = authenticate(request,username = username , password = password)
 
-        if user:
+        if user is not None:
+            
+            login(request, user)
 
-            login(request,user)
+            role = user.role
+            print(type(user.role))
+            print(user.role)
 
-            if user.role == "ADMIN":
+            if role == "ADMIN":
                 return redirect("admin_dashboard")
-
-            if user.role == "HR":
+            
+            elif role == "HR":
                 return redirect("hr_dashboard")
-
-            if user.role == "MANAGER":
+            
+            elif role == "MANAGER":
                 return redirect("manager_dashboard")
-
-            if user.role == "EMPLOYEE":
+            
+            elif role == "EMPLOYEE":
                 return redirect("employee_dashboard")
+        
+        else:
+            messages.error(request, "Invalid Username or password")
 
-    return render(request,"login.html")
+
+    return render(request,'accounts/login.html')
+
+
+def admin_dashboard(request):
+    return render(request,"dashboard/admin_dashboard.html")
+
+
+def hr_dashboard(request):
+    return render(request, "dashboard/hr_dashboard.html")
+
+
+def manager_dashboard(request):
+    return render(request, "dashboard/manager_dashboard.html")
+
+
+def employee_dashboard(request):
+    return render(request, "dashboard/employee_dashboard.html")
