3763450fff2d7e00f8dad98d6c820294144d6b5b
coder-ravi100 <ravirathod100100@gmail.com>
2026-03-17 22:26:53 +0530

    Attendance Logic Code Is Start

--- core/models.py
+++ core/models.py
@@ -126,3 +126,25 @@ class Leave(models.Model):
 
     def __str__(self):
         return self.employee.username
+    
+
+
+class Attendance(models.Model):
+
+    STATUS = [
+        ("Present", "Present"),
+        ("Absent", "Absent"),
+        ("Half Day", "Half Day"),
+    ]
+
+    employee = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    date = models.DateField(auto_now_add=True)
+
+    check_in = models.TimeField(null=True, blank=True)
+    check_out = models.TimeField(null=True, blank=True)
+
+    status = models.CharField(max_length=20, choices=STATUS)
+
+    def __str__(self):
+        return f"{self.employee.username} - {self.date}"
