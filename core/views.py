18047c241e3229911ff39acce131223921772e4a
coder-ravi100 <ravirathod100100@gmail.com>
2026-04-01 23:43:18 +0530

    paginator tool add

--- core/views.py
+++ core/views.py
@@ -13,7 +13,7 @@ import random
 
 from .utils import *
 from django.utils.timezone import localtime
-from django.db.models  import Count
+from django.db.models import Count
 from .utils import sendmailForOtp
 import json
 from django.core.paginator import Paginator
@@ -422,15 +422,9 @@ def add_employee(request):
         messages.success(request, "Employee added successfully")
         return redirect('list-employee')
 
-    #Paginator Logic Code  
-    employees = EmployeeProfile.objects.all()
-    paginator = Paginator(employees, 3)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
 
     context = {
-        'departments': departments,
-        'page_obj': page_obj
+        'departments': departments
     }
     return render(request, 'employee/Employee_add.html', context)
 
